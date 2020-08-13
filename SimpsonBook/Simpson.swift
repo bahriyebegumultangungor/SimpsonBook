8859d269d850222b989e99017a4e82c930736735
Bahriye Begum Ultan Gungor <begumultan@gmail.com>
2020-08-13 13:18:08 +0300

    first commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,26 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by BEGÜM on 13.08.2020.
+//  Copyright © 2020 Bahriye Begum Ultan Gungor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+    }
+    
+}
